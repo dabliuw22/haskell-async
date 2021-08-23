81ee4344fa46a599424be526c4e027ea36d53438
Will Leyton <wleylop22@gmail.com>
2021-08-23 09:01:57 -0500

    Update

--- src/Async/AsyncMain.hs
+++ src/Async/AsyncMain.hs
@@ -4,9 +4,9 @@ import Async.Task
 import Control.Concurrent (threadDelay)
 
 async :: IO ()
-async = do 
+async = do
   v <- run $ action "Hello World"
   print v
 
 action :: String -> IO String
-action s = do threadDelay 100 >>= \ x -> return s
+action s = do threadDelay 100 >>= \x -> return s
