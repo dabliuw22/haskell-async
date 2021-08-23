4e5db7ea0b1a6211528d575fa1ae0d6b0b406470
Will Leyton <wleylop22@gmail.com>
2021-08-23 09:01:31 -0500

    Update

--- src/Async/Task.hs
+++ src/Async/Task.hs
@@ -3,9 +3,9 @@ module Async.Task where
 import Control.Concurrent.Async
 
 run :: IO String -> IO String
-run action = do 
+run action = do
   mVar <- async action
-  result <- waitCatch mVar--wait mVar
+  result <- waitCatch mVar --wait mVar
   case result of
     Right a -> return a
-    Left e  -> return "Error"
+    Left e -> return "Error"
