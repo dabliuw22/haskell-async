79ef50c49e2b2be7e07b434f8afc91b4cb24cdae
Will Leyton <wleylop22@gmail.com>
2021-08-25 08:32:54 -0500

    Update SumIO

--- src/Parallel/SumIO.hs
+++ src/Parallel/SumIO.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+
 module Parallel.SumIO where
 
 import qualified Control.Concurrent.ParallelIO.Global as G
@@ -10,13 +11,17 @@ parSumIO :: [Int] -> IO Int
 parSumIO list = pure $ parSum list
 
 execWithLocalPool :: L.Pool -> IO Int
-execWithLocalPool pool = L.parallel pool [parSumIO [1, 2, 3], parSumIO [4, 5, 6]] >>= 
-  (\case
-     (h : t) -> pure $ sum (h : t)
-     _ -> pure 0)
-       
+execWithLocalPool pool =
+  L.parallel pool [parSumIO [1, 2, 3], parSumIO [4, 5, 6]]
+    >>= ( \case
+            (h : t) -> pure $ sum (h : t)
+            _ -> pure 0
+        )
+
 execWithImplicitGlobalPool :: IO Int
-execWithImplicitGlobalPool = G.parallel [parSumIO [1, 2, 3], parSumIO [4, 5, 6]] >>=
-    (\case
-       (h : t) -> pure $ sum (h : t)
-       _ -> pure 0)
+execWithImplicitGlobalPool =
+  G.parallel [parSumIO [1, 2, 3], parSumIO [4, 5, 6]]
+    >>= ( \case
+            (h : t) -> pure $ sum (h : t)
+            _ -> pure 0
+        )
